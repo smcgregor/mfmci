599cff6f08e7aff51693d19f67613ff745fcc058
Sean McGregor <smcgregor08@cmc.edu>
2016-09-12 13:43:27 -0700

    added fuel summaries to landscape summaries

--- post_process.py
+++ post_process.py
@@ -3,6 +3,8 @@ from subprocess import call
 from struct import unpack
 import pickle
 import nose.tools
+import bz2
+import random
 
 def lcpStateSummary(landscapeFileName):
     """
@@ -10,15 +12,25 @@ def lcpStateSummary(landscapeFileName):
     Landscapes are 940X1127X10=10593800 shorts (11653180)
     :param landscapeFileName: The name of the landscape we want to generate a state summary for.
     :return: array of values for distance metric variables
+
+    Fuel Model
+    Canopy Closure
+    Canopy Height start
+    Canopy Base Height start
+    Canopy Bulk Density
+    Covertype
+    Stand Density Index
+    Succession Class
+    Maximum Time in State
+    Stand Volume Age
+    highFuel
+    modFuel
+    lowFuel
+    percentHighFuel
     """
     distanceMetricVariableCount = 10
 
-    # tmpFileName
-    decompressedFilename = "/nfs/eecs-fserv/share/mcgregse/tmp/tmp.lcp." + landscapeFileName.split("/")[-1]
-
-    call(["bzip2 " + landscapeFileName + " -dkc > " + decompressedFilename], shell=True)
-
-    lcpFile = file(decompressedFilename, "rb")
+    lcpFile = bz2.BZ2File(landscapeFileName, "r")
     print "processing %s" % lcpFile
     layers = []
     for idx in range(0,distanceMetricVariableCount):
@@ -33,23 +45,35 @@ def lcpStateSummary(landscapeFileName):
         shortCount += 1
         shortBytes = lcpFile.read(2)
     lcpFile.close()
+    highFuel = 0
+    modFuel = 0
+    lowFuel = 0
     summary = []
     for layerIdx, layer in enumerate(layers):
         average = 0
         for idx, pixel in enumerate(layers[layerIdx]):
+            if layerIdx == 0:
+                if pixel == 122 or pixel == 145:
+                    highFuel += 1
+                elif pixel == 121 or pixel == 186:
+                    modFuel += 1
+                elif pixel == 142 or pixel == 161 or pixel == 187 or pixel == 184 or pixel == 185:
+                    lowFuel += 1
             average = float(average * idx + pixel)/(idx + 1.)
         summary.append(average)
-    print "removing file {}".format(decompressedFilename)
-    call(["rm " + decompressedFilename], shell=True) # cleanup decompressed file
     del summary[-1] # remove the last element because it is not needed
+    summary.append(highFuel)
+    summary.append(modFuel)
+    summary.append(lowFuel)
+    summary.append(float(highFuel)/(highFuel+modFuel+lowFuel))
     return summary
 
 def test_post_process_landscapes():
     """
     Generate pickled version of all the state summaries for each of the landscapes in the landscapes directory
     """
-    landscapeDirectory = "/nfs/eecs-fserv/share/rhoutman/FireWoman/results/landscapes"
-    resultsDirectory = "/nfs/eecs-fserv/share/mcgregse/landscape_summaries"
+    landscapeDirectory = "/nfs/eecs-fserv/share/rhoutman/FireWoman/results/landscapes/"
+    resultsDirectory = "/nfs/eecs-fserv/share/mcgregse/landscape_summaries/"
     allFiles = os.listdir(landscapeDirectory)
     currentlyOutputFiles = os.listdir(resultsDirectory)
     files = []
@@ -64,20 +88,21 @@ def test_post_process_landscapes():
             files.append(filename)
 
     print "processing {} files".format(len(files))
-    fileNum = len(files)-1
-    while fileNum >= 0:
+    fileNum = int(100*random.random())
+    #fileNum = 0
+    while fileNum < len(files):
         f = files[fileNum]
 
         print "processing {}".format(f)
         if os.path.isfile(resultsDirectory+f):
-            print "skipping forward {} since this landscape is processed".format(10)
-            fileNum += 10
+            print "skipping forward since this landscape is processed"
+            fileNum += int(200*random.random())
             continue
         try:
             s = lcpStateSummary(landscapeDirectory+f)
             if os.path.isfile(resultsDirectory+f):
-                print "skipping forward {} since this landscape is processed".format(10)
-                fileNum += 10
+                print "skipping forward since this landscape is processed"
+                fileNum += int(200*random.random())
                 continue
             out = open(resultsDirectory+f, "wb")
             pickle.dump(s, out)
@@ -86,14 +111,14 @@ def test_post_process_landscapes():
             print type(inst)
             print inst.args
             print "failed to summarize: {}".format(f)
-        fileNum += (-1)
+        fileNum += 1
 
 
 def test_check_for_incomplete_pickles():
     """
     Open all the landscape pickles and check that they are properly formatted. Print the pickles that are not well formatted.
     """
-    resultsDirectory = "/nfs/eecs-fserv/share/mcgregse/landscape_summaries"
+    resultsDirectory = "/scratch/mcgregse/aaai/landscape_summaries/"
     files = os.listdir(resultsDirectory)
     fileNum = 0
     while fileNum < len(files):
